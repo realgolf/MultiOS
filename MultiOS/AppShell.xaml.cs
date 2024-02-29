021fba38438d62aa78689febbce4dab7a3664ee5
Julian Hammer <jh@moinjulian.com>
2024-02-29 12:51:10 +0000

    Initial MultiOS with C# ⛳️

---
+++ MultiOS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiOS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MultiOS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MultiOS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
